d5b6d15c1a59d4867d361cf9a390f8e95af19a4b
ImanAdithya <adithyaiman07@gmail.com>
2024-03-16 09:36:26 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [g, setG] = useState(0);
+  const [f, setF] = useState(0);
+  const [c, setC] = useState(0);
+
+  const [answer, setAnswer] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+
+        <div className='main-div'>
+          <h1>x<sup>2</sup> + y<sup>2</sup> + 2</h1>
+          <input onChange={(e) => {setG(parseInt(e.target.value))}}
+          />
+          <h1>x + 2 </h1>
+          <input onChange={(e) => {setF(parseInt(e.target.value))}}
+          />
+          <h1>y + </h1>
+          <input onChange={(e) => {setC(parseInt(e.target.value))}}
+          />
+          <h1>= 0</h1>
+        </div>
+
+        <button onClick={
+          () => {
+            let value = Math.sqrt((g*g + f*f - c));
+            setAnswer(value);
+          }
+        }>Calculate</button>
+
+        <div className='main-div'>
+          <h1>Radius = </h1>
+          <h1>{answer}</h1>
+        </div>
+
+
+      </div>
+
   );
 }
 
